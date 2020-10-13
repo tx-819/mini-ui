75f6d292fa47377af39a67d807e3484f84b20114
tang <923233700@qq.com>
2020-10-13 10:25:43 +0800

    添加 icon 和 iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,9 +1,18 @@
 <template>
-  <button class="m-button">按钮</button>
+  <button class="m-button" :class="{ [`icon-${iconPosition}`]: true }">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {};
+export default {
+  props: ["icon", "iconPosition"],
+};
 </script>
 
 <style lang="scss" scoped>
@@ -14,6 +23,10 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
   &:hover {
     border-color: var(--border-color);
   }
@@ -22,6 +35,23 @@ export default {};
   }
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
+    }
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
