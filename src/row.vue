fe08b3e858963b0cd76bcd4b8f0bbd4a8a2636b2
tang <923233700@qq.com>
2021-02-20 20:54:46 +0800

    row 和 col 的雏形完成

---
+++ src/row.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MiniRow',
+}
+</script>
+<style lang="scss" scoped>
+.row{
+  display: flex;
+}
+</style>
