9ab03c5dfb62f5a9371e534b8a9b2c957450040f
tang <923233700@qq.com>
2021-02-22 19:52:21 +0800

    添加 row 的 align

--- src/row.vue
+++ src/row.vue
@@ -1,31 +1,53 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
 <script>
 export default {
-  name: 'MiniRow',
+  name: "MiniRow",
   props: {
     gutter: {
-      type: [Number, String]
-    }
+      type: [Number, String],
+    },
+    align: {
+      type: String,
+      validator(value) {
+        return ["left", "right", "center"].includes(value);
+      },
+    },
   },
   mounted() {
     this.$children.forEach((vm) => {
-      vm.gutter = this.gutter
-    })
+      vm.gutter = this.gutter;
+    });
   },
   computed: {
     rowStyle() {
-      let {gutter} = this
-      return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
-    }
-  }
-}
+      let { gutter } = this;
+      return {
+        marginLeft: -gutter / 2 + "px",
+        marginRight: -gutter / 2 + "px",
+      };
+    },
+    rowClass() {
+      let { align } = this;
+      return [align && `align-${align}`];
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
 .row {
   display: flex;
+  &.align-left{
+    justify-content: flex-start;
+  }
+  &.align-center{
+    justify-content: center;
+  }
+  &.align-right{
+    justify-content: flex-end;
+  }
 }
 </style>
