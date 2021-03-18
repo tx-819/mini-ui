1a8d48e903d417163da0dd5b1aa86ccb39c4cf41
tang <923233700@qq.com>
2021-03-18 23:04:10 +0800

    解决 currentToast 的一个bug

--- src/plugin.js
+++ src/plugin.js
@@ -8,17 +8,22 @@ export default {
             if (currentToast) {
                 currentToast.close()
             }
-            currentToast = creatToast({Vue, message, propsData: toastOptions})
+            currentToast = creatToast({
+                Vue, message, propsData: toastOptions, onClose: () => {
+                    currentToast = null
+                }
+            })
         }
     }
 }
 
 
-function creatToast({Vue, message, propsData}) {
+function creatToast({Vue, message, propsData, onClose}) {
     const Constructor = Vue.extend(Toast)
     let toast = new Constructor({propsData})
     toast.$slots.default = [message]
     toast.$mount()
+    toast.$on('close', onClose)
     document.body.appendChild(toast.$el)
     return toast
 }
--- src/toast.vue
+++ src/toast.vue
@@ -69,6 +69,7 @@ export default {
     },
     close() {
       this.$el.remove()
+      this.$emit('close')
       this.$destroy()
     },
     onClickClose() {
